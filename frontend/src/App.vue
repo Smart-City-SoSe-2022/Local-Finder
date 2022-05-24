e98522af32e615c86b4140d880f1251f3921c079
René Braun <rene.braun@fh-bielefeld.de>
2022-05-24 11:15:04 +0200

    Bound previous Commits together in App and Router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <div id="body">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div id="body">
+    <Header />
+    <div v-show="this.debug">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/results">Ergebnisse</router-link>
+    </div>
     <router-view/>
-  </div>
-  
+    </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+name: 'Home',
+components: {
+Header
+},
+    data() {
+        return {
+        debug: true
+        }
+    },
+}
+</script>
 
 <style>
 html {
-  background: #eef;
+    background: #eef;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #body {
-  max-width: 70ch;
-  padding: 3em 1em;
-  margin: auto;
-  line-height: 1.75;
-  font-size: 1.2em;
+    max-width: 80ch;
+    padding: 3em 1em;
+    margin: auto;
+    line-height: 1.75;
+    font-size: 1.3em;
 }
 /* Optional Header Margins
 h1,h2,h3,h4,h5,h6 {
@@ -41,15 +60,23 @@ p,ul,ol {
 
 /* Refactor Nav somewhere else */
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
+}
+
+.button {
+    background: cornflowerblue;
+    padding: 5px;
+    border-radius: 5px;
+    font-weight: 500;
+    font-size: 1.5ch;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Results from '../views/Results.vue'
+import About from '../views/About.vue'
+import LocalPage from '../views/LocalPage.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/results',
+        name: 'Results',
+        component: Results
+    },
+    {
+        path: '/local/:id',
+        name: 'LocalPage',
+        component: LocalPage,
+        params: true
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
