35f863d088fb8167697a1340d26f4628418e460a
René Braun <rene.braun@fh-bielefeld.de>
2022-05-24 11:13:52 +0200

    Added a LocalPageView that show details of an specific local

---
+++ frontend/src/views/LocalPage.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1> {{this.location.name}}</h1>
+    <p>{{this.location.address}}</p>
+    <p>{{this.location.open}}</p>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'LocalPage',
+    data() {
+        return {
+            location:
+                {
+                    "id": 1,
+                    "name": "Loc1",
+                    "address": "Straße Da 1",
+                    "open": "17 Uhr"
+                }
+        }
+    },
+}
+</script>
