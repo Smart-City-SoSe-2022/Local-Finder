c0c3b88e81387f5910195e8083c15a90618527ce
René Braun <rene.braun@fh-bielefeld.de>
2022-05-24 11:11:26 +0200

    Added a Header with the search form

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,42 @@
+<template>
+<div> 
+    <div id="search">
+        <form>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <button class="button" type="button"> Suche starten!</button>
+        </form>
+        
+    </div>
+    <div>
+        
+    </div>
+</div>
+    
+</template>
+
+<style scoped>
+    #search {
+        background: #ddf;
+        border-radius: 10px;
+        padding: 10px 0;
+    }
+    form {
+        
+        margin: 0 auto;
+    }
+    form > input {
+        display: block;
+        min-width: 35ch;
+        margin: 10px auto; 
+        padding: 5px 10px;
+        font-size: 2ch;
+        border: 2px solid #ddf;
+        border-radius: 10px;
+    }
+
+    form > input:focus {
+        border: 2px solid black;
+    }
+</style>
