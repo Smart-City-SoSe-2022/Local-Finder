cc0ed2d6117157407db7889e1cfdb276e4abf799
René Braun <rene.braun@fh-bielefeld.de>
2022-05-29 19:02:26 +0200

    Added env-file and adjusted requests

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 import json
-from flask import Flask, jsonify, render_template, url_for, request
+from flask import Flask, jsonify, render_template, request_started, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 from dbModels import db, Account, Reservation, Lokal
-from rabbit import reciever, rabbit_route
+from rabbit import receiver_local_status, rabbit_route, receiver
+from dotenv import dotenv_values
 
+config = dotenv_values(".env.cfg")
 # flask db migrate
 # flask db upgrade
 
@@ -21,8 +23,8 @@ app.register_blueprint(rabbit_route)
 
 ### Postgre Database
 
-# Seting up database connection         'postgresql://<username>:<password>@<server>:5432/<db_name>'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/localfinder'
+# Seting up database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = config['DB_FULL_URI']
 
 # If set to True (the default) Flask-SQLAlchemy will track modifications of objects and emit signals. 
 # This requires extra memory and can be disabled if not needed.
@@ -32,8 +34,8 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 ### Rabbit MQ
-reciever.start()
-
+receiver_local_status.start()
+receiver.start()
 ##### Routing Paths #####
 
 @app.route('/')
@@ -47,29 +49,43 @@ def ping_pong():
     except:
         return 'Faield to commit to the Database. app.route/ping'
 
-@app.route('/api/newAccount')
-def new_account():
-    name = request.args.get('name')
-    print(name)
-    age = request.args.get('age')
-    print(age)
-    acc = Account(name=name, age=age)
-    try: 
-        db.session.add(acc)
-        db.session.commit()
-    except: 
-        return "Fehler: Account konnte nicht angelegt werden..."
-    return name + " erfolgreich angelegt!"
+# @app.route('/api/newAccount')
+# def new_account():
+#     name = request.args.get('name')
+#     print(name)
+#     age = request.args.get('age')
+#     print(age)
+#     acc = Account(name=name, age=age)
+#     try: 
+#         db.session.add(acc)
+#         db.session.commit()
+#     except: 
+#         return "Fehler: Account konnte nicht angelegt werden..."
+#     return name + " erfolgreich angelegt!"
 
-@app.route('/api/getAccounts')
-def get_accounts():
-    # Account.query.filter_by(name="Peter").first()
-    accs = db.session.query(Account)
-    if accs:
-        print(accs.all())
-        return "Printed in Server Log"
-    else:
-        return "No accounts found"
+# @app.route('/api/getAccounts')
+# def get_accounts():
+#     # Account.query.filter_by(name="Peter").first()
+#     accs = db.session.query(Account)
+#     if accs:
+#         print(accs.all())
+#         return "Printed in Server Log"
+#     else:
+#         return "No accounts found"
+
+@app.route('/api/search')
+def search_request():
+    #destination = request.args.get('destination')
+    pass
+
+@app.route('/api/downloadFile')
+def download_file():
+    #file = request.args.get('file')
+    pass
+
+@app.route('/api/requestReservation')
+def request_reservation():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/rabbit.py
+++ backend/rabbit.py
@@ -1,33 +1,78 @@
 import json
 import pika
 import threading
-from flask import Blueprint
+from flask import Flask, jsonify, make_response, render_template, request_started, url_for, request, Blueprint
+from dbModels import db, Lokal
+from dotenv import dotenv_values
 
-exchange = 'microservice.eventbus'
-host = 'localhost'
+config = dotenv_values(".env.cfg")
+for x in config:
+    print(x)
+
+exchange = config['EXCHANGE']
+queue = config['QUEUE']
+host = config['HOST']
 
 rabbit_route = Blueprint('rabbit_route', __name__)
 
-@rabbit_route.route('/api/rabbit', methods=['GET'])
-def rabbit_test():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='local.auth.create')
-    msg = '{"name": "Peter","age": 25, "foo": "bar"}'
-    channel.basic_publish(exchange='microservice.eventbus', routing_key='local.auth.create', body=msg)
-    print(json.dumps(msg))
-    connection.close()
-    return msg
-
-def reciever_thred():
+def send(key, body:str):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host))
     channel = connection.channel()
-    channel.queue_declare(queue='local.auth.status')
-    channel.queue_bind(
-        exchange='microservice.eventbus', queue='local.auth.status', routing_key="*.#")
-    def callback(ch, method, properties, body):
-        print(' - Erhalten : %r' % body)
-    channel.basic_consume(queue='local.auth.status', on_message_callback=callback, auto_ack=True )
+    channel.queue_declare(queue=queue)
+    channel.basic_publish(exchange=exchange, routing_key=key, body=body)
+    connection.close()
+
+def receive(key, callback):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+    channel = connection.channel()
+    channel.queue_declare(queue=queue)
+    channel.exchange_declare(exchange='microservice.eventbus', exchange_type='topic')
+    channel.queue_bind(exchange=exchange, queue=queue, routing_key=key)
+    channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True )
     channel.start_consuming()
 
-reciever = threading.Thread(target=reciever_thred)
+# Sending route for new location authentication
+@rabbit_route.route('/api/requestLocal', methods=['POST'])
+def request_local():
+    if request.method != 'POST':
+        return "Not a POST method"
+    body = request.get_json()
+    send(key='auth.create', body=json.dumps(body))
+    return make_response("ok")
+
+# Receiver for answer from Stadtverwaltung
+def local_status():
+    def callback(ch, method, properties, body):
+        
+        lok = Lokal(owner=body.ownerId)
+        try: 
+            db.session.add(lok)
+            db.session.commit()
+        except: 
+            return "Fehler: Account konnte nicht angelegt werden..."
+        print(' - Erhalten : %r' % body)
+    receive('auth.status', callback)
+
+# Test route for rabbitMQ
+@rabbit_route.route('/api/rabbit', methods=['GET'])
+def rabbit_test():
+    msg = '{"name": "Peter","age": 25, "foo": "bar"}'
+    send('hallo', msg)
+    print(json.dumps(msg))
+    return msg
+
+# Test listener for rabbitMQ
+def test_reviever():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+    channel = connection.channel()
+    channel.queue_declare(queue=queue)
+    channel.queue_bind(
+        exchange='microservice.eventbus', queue=queue, routing_key="*.#")
+    def callback(ch, method, properties, body):
+        print(' - Erhalten : %r' % body)
+    channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True )
+    channel.start_consuming()
+
+# Receiver Threads, start in App.py
+receiver = threading.Thread(target=test_reviever)
+receiver_local_status = threading.Thread(target=local_status)
