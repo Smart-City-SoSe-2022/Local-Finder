246a875a3f26cd1174423cca9735523e9cf4c6f9
René Braun <rene.braun@fh-bielefeld.de>
2022-05-31 13:59:21 +0200

    added CreateAcc and DeleteAcc functions

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,11 @@
 import json
-from flask import Flask, jsonify, render_template, request_started, url_for, request
+from flask import Flask, jsonify, make_response, render_template, request_started, session, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 from dbModels import db, Account, Reservation, Lokal
 from rabbit import receiver_local_status, rabbit_route, receiver
 from dotenv import dotenv_values
-
-config = dotenv_values(".env.cfg")
 
 # To combine the frontend-build with the backend,
 # I changed the default static and template folders 
@@ -18,9 +16,11 @@ app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
 app.register_blueprint(rabbit_route)
+config = dotenv_values(".env.cfg")
 
-### Postgre Database
-
+""""""""""""""""""""""" 
+    Postgre Database 
+"""""""""""""""""""""""
 # Seting up database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = config['DB_FULL_URI']
 
@@ -31,11 +31,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
-### Rabbit MQ Lister Threads
+"""""""""""""""""""""""""""""""""""
+    Rabbit MQ Lister Threads 
+"""""""""""""""""""""""""""""""""""
 receiver_local_status.start()
 receiver.start()
 
-##### Routing Paths #####
+
+"""""""""""""""""""""""
+    Routing Paths 
+"""""""""""""""""""""""
 
 @app.route('/')
 def index():
@@ -43,7 +48,7 @@ def index():
 
 @app.route('/api/ping', methods=['GET'])
 def ping_pong():
-    try: 
+    try:
         return jsonify('pong')
     except:
         return 'Faield to commit to the Database. app.route/ping'
@@ -61,6 +66,31 @@ def download_file():
 @app.route('/api/requestReservation')
 def request_reservation():
     pass
+
+@app.route('/api/createAccount', methods=['POST'])
+def create_account():
+    if request.method != 'POST':
+        return make_response("Request not a POST method")
+    body = request.get_json()
+    newAcc = Account(name=body["lastname"], street=body["address"], plz=body["plz"])
+    try: 
+        db.session.add(newAcc)
+        db.session.commit()
+    except:
+        return make_response("ERROR accured. Couldn't create Account")
+    return make_response("Account Created")
+
+@app.route('/api/deleteAccount', methods=['DELETE'])
+def delete_account():
+    if (request.method != 'DELETE'):
+        return make_response('Request not a DELETE method')
+    body = request.get_json()
+    delAcc = db.session.query(Account).get(body["id"])
+    if not delAcc:
+        return make_response("Account doesn't exist.")
+    db.session.delete(delAcc)
+    db.session.commit()
+    return make_response("Account deletet.")
 
 if __name__ == '__main__':
     app.run(debug=True)
