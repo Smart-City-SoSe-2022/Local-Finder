fde4b241451c23ba20ee94a0731715c303637a08
René Braun <rene.braun@fh-bielefeld.de>
2022-05-31 13:58:35 +0200

    updated database models

--- backend/dbModels.py
+++ backend/dbModels.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Table, Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
@@ -22,27 +23,34 @@ class Account(db.Model):
     __tablename__ = 'account'
     _id = Column("id", Integer, primary_key=True)
     name = Column("name", String, nullable=False)
-    age = Column("age", Integer)
+    street = Column("street", String)
+    plz = Column("plz", String(5))
     #favorites = relationship('Lokal', secondary=favTable, backref='followers')
     # ownerLokal = relationship('Lokal', back_populates="account", uselist=False)
     # reservation_id = Column(Integer, ForeignKey('reservation.id'))
-    # reservation = relationship("Reservation")
-    def __init__(self, name, age):
+    reservation = relationship("Reservation")
+    ownesLokal = relationship("Lokal")
+    def __init__(self, name, street, plz):
         self.name = name
-        self.age = age
+        self.street = street
+        self.plz = plz
     def __repr__(self):
         return f"{self.name} - Age:{self.age}"
 
 class Reservation(db.Model):
     __tablename__ = 'reservation'
-    id = Column(Integer, primary_key=True)
-    personNumber = Column(Integer)
+    _id = Column("id", Integer, primary_key=True)
     datetime = Column(DateTime, default = datetime.utcnow)
     accepted = Column(Boolean, default = False)
+    reservedBy = Column(Integer, ForeignKey("account.id"))
+    reservedLocal = Column(Integer, ForeignKey("lokal.id"))
 
 class Lokal(db.Model):
     __tablename__ = 'lokal'
-    id = Column(Integer, primary_key=True)
-    # account = relationship("Account")
-    # ownedBy = relationship("Account", back_populates="lokal")
+    _id = Column("id", Integer, primary_key=True)
+    name = Column("name", String)
+    ownedBy = Column(Integer, ForeignKey("account.id"))
+    reservation = relationship("Reservation")
+    def __init__(self, owner):
+        self.ownedBy = owner
 
